5ee5a5779cddba7180ec70762a22a15a31822e20
Tyler <tn5421@gmail.com>
2019-05-24 06:35:17 -0500

    attempted to fix, but broke it even more

--- main.py
+++ main.py
@@ -3,41 +3,21 @@ import discord
 import time
 import asyncio
 
+from wkai import *
 from discord.ext import commands
 
 description = """Billy vs Snakeman InfoBot by tn5421"""
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix, description=description)
 
-startup_extensions =  ["wkai"]
-
-if __name__ == "__main__":
-    for ext in startup_extensions:
-        try:
-            bot.load_extension(ext)
-        except Exception as e:
-            print(f"Failed to load extension {ext}\n{type(e).__name__}: {e}")
-
-@bot.command()
-async def load(ctx, extension_name:str=None):
-    """
-    Loads an extension.
-
-    Valid extensions are:
-        wkai
-    """
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("`*`*`py\n{}: {}\n`*`*`".format(type(e).__name__, str(e)), delete_after=5)
-        await asyncio.sleep(4)
-        await bot.delete_message(ctx.message)
-        return
-
-    await bot.say(f"Loaded extension: __{extension_name}__")
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+wkai()
+
+@bot.command()
+adam()
 
 bot.run("a token")
--- wkai.py
+++ wkai.py
@@ -5,28 +5,23 @@ import asyncio
 
 from discord.ext import commands
 
-def setup(client):
-    client.add_cog(wkai(client))
-
-description = """Billy vs Snakeman InfoBot by tn5421"""
-prefix = "!"
-client = commands.Bot(command_prefix=prefix, description=description)
-
 wk_list = ["Craftworld at War", "AspenStory", "ADAM", "FarmVale", "ForeverQuest"]
 rday = (int(time.strftime("%d")))
 wk_day = rday % 5
 
-@client.command()
-async def wkai(ctx):
-    day_wkai = "Today's WKAI summon is a "
-    day_wkai += wk_list[wk_day]
-    await ctx.send(day_wkai)
+def wkai():
+    async def wkai(ctx):
+        day_wkai = "Today's WKAI summon is a "
+        day_wkai += wk_list[wk_day]
+        await ctx.send(day_wkai)
+        pass
 
-@client.command()
-async def adam(ctx):
-    adam_wkai = "The next ADAM summoning is in "
-    if wk_day == 2:
-        adam_wkai += "5 Days"
-    else:
-        adam_wkai += (str((2 - rday) % 5)) + " Days"
-    await ctx.send(adam_wkai)
+def adam ():
+    async def adam(ctx):
+        adam_wkai = "The next ADAM summoning is in "
+        if wk_day == 2:
+            adam_wkai += "5 Days"
+        else:
+            adam_wkai += (str((2 - rday) % 5)) + " Days"
+        await ctx.send(adam_wkai)
+        pass
