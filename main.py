a3145387369bcebc28d26f08a37821c246efeb9c
Tyler <tn5421@gmail.com>
2019-06-11 21:31:16 -0500

    when u accidentall push a typo to master

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 settings = json.loads(open('settings.json').read())
 token = settings['token']
-version = [settings['version']
+version = settings['version']
 # this comment only here because the interpreter is actually toxic awful garbage cancer
 description = """Billy vs Snakeman InfoBot by tn5421"""
 prefix = "!"
