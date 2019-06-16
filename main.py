f64587b0153aad2aa2cf3a23d4541120f5300cda
tn5421 <tn5421@gmail.com>
2019-06-16 07:17:18 -0500

    fix command logic, extend commands

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ bot = commands.Bot(command_prefix=prefix, description=description)
 
 wk_list = ["Craftworld at War", "AspenStory", "ADAM", "FarmVale", "ForeverQuest"]
 
+def distance(list1, list2):
+    i = (list1 - list2) % 5
+    j = (list2 - list1) % 5
+    return min(i, j)
+
+def get_rday():
+    return (int(time.strftime("%d")))
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
@@ -23,24 +31,69 @@ async def ping(ctx):
 @bot.command()
 async def wkai(ctx):
     day_wkai = "Today's WKAI summon is a "
-    def get_rday():
-        return (int(time.strftime("%d")))
+
     wk_day = get_rday() % 5
     day_wkai += wk_list[wk_day]
     await ctx.send(day_wkai)
     pass
 
 @bot.command()
+async def cow(ctx):
+    cow_wkai = "The next Craftworld of War summoning is in "
+    wk_day = get_rday() % 5
+    if wk_day == 0:
+        cow_wkai += "5 Days"
+    else:
+        cow_wkai += (str(5 - wk_day)) + " Day(s)"
+    await ctx.send(cow_wkai)
+    pass
+
+@bot.command()
+async def aspen(ctx):
+    aspen_wkai = "The next AspenStory summoning is in "
+    wk_day = get_rday() % 5
+    wk_distance = distance(wk_day, 1)
+    if wk_day == 1:
+        aspen_wkai += "5 Days"
+    else:
+        aspen_wkai += (str(wk_distance)) + " Day(s)"
+    await ctx.send(aspen_wkai)
+    pass
+
+@bot.command()
 async def adam(ctx):
     adam_wkai = "The next ADAM summoning is in "
-    def get_rday():
-        return (int(time.strftime("%d")))
     wk_day = get_rday() % 5
+    wk_distance = distance(wk_day, 2)
     if wk_day == 2:
         adam_wkai += "5 Days"
     else:
-        adam_wkai += (str((2 - get_rday()) % 5)) + " Day(s)"
+        adam_wkai += (str(wk_distance)) + " Day(s)"
     await ctx.send(adam_wkai)
+    pass
+
+@bot.command()
+async def farm(ctx):
+    farm_wkai = "The next FarmVale summoning is in "
+    wk_day = get_rday() % 5
+    wk_distance = distance(wk_day, 3)
+    if wk_day == 3:
+        farm_wkai += "5 Days"
+    else:
+        farm_wkai += (str(wk_distance)) + " Day(s)"
+    await ctx.send(farm_wkai)
+    pass
+
+@bot.command()
+async def fq(ctx):
+    fq_wkai = "The next ForeverQuest summoning is in "
+    wk_day = get_rday() % 5
+    wk_distance = distance(wk_day, 4)
+    if wk_day == 4:
+        fq_wkai += "5 Days"
+    else:
+        fq_wkai += (str(wk_distance)) + " Day(s)"
+    await ctx.send(fq_wkai)
     pass
 
 @bot.command()
